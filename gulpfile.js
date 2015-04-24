8a7240020841ea63561a6fe5eaf7b885309830ed
Jan Strassburg <jan.strassburg@gmail.com>
2015-04-24 12:35:24 +0200

    rtert

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var html2js = require('gulp-html-js-template');
 
 var paths = {
+	templates: [
+		'src/views/*.html'
+	],
 	scripts: [
+		'dist/templates.js',
 		'src/js/*.js'
 	]
 };
 
+gulp.task('templates', function () {
+	return gulp.src(paths.templates)
+		.pipe(html2js())
+		.pipe(concat('templates.js'))
+		.pipe(gulp.dest('dist'));
+});
+
 gulp.task('scripts', function () {
 	return gulp.src(paths.scripts)
-		.pipe(uglify())
 		.pipe(concat('all.min.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', function () {
+	gulp.watch(paths.templates, ['templates']);
 	gulp.watch(paths.scripts, ['scripts']);
 });
